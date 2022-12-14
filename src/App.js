99c65ee9b9f7bd691e933e48b595d847753bc7a1
Pranav Teja <86045290+Pranavtej@users.noreply.github.com>
2022-12-14 11:33:34 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         {/* <img src="Octocat.png" className="App-logo" alt="logo" /> */}
         <p>
-        <span className="heart">&lt; </span>d<span className="heart"> /</span> D <span className="heart">&gt;</span>
-        </p>
+        <code><span className="heart">&lt; </span>d<span className="heart"> /</span> D <span className="heart">&gt;</span></code>
+        </p> 
         <p className="small">
           <code>Developer by Developers</code></p>
         
